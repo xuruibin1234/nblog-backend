23385b0cf2c3b7311f93af37e4fac6ac18381ff0
xuruibin1234 <17815708315@163.com>
2024-05-14 15:25:15 +0800

    微服务父子项目骨架

---
+++ cloud-user/src/main/java/cn/xrb/clouduser/CloudUserApplication.java
@@ -0,0 +1,13 @@
+package cn.xrb.clouduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudUserApplication.class, args);
+    }
+
+}
---
+++ cloud-user/src/test/java/cn/xrb/clouduser/CloudUserApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xrb.clouduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
